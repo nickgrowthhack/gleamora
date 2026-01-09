48636cc85066fe9bff4e56444fe074a1460ed890
Nick <nickgrowthhack@gmail.com>
2026-01-09 12:28:37 -0300

    fix(layout): suppress hydration warning in body element

--- src/app/layout.js
+++ src/app/layout.js
@@ -47,6 +47,7 @@ export default async function RootLayout({ children }) {
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
       </head>
       <body
+        suppressHydrationWarning
         className={cn(
           geistSans.variable,
           geistMono.variable,
